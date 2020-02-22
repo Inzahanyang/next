bab3dfb2c59b373e01438dcaa9152d2eaf1796dd
inzahanyang <inzahanyang@gmai.com>
2020-02-22 21:52:09 +0900

    first

---
+++ 2-using-shared-components/pages/index.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+import Header from '../comps/Header';
+import Layout from '../comps/MyLayout';
+
+export default function Index() {
+  return (
+    <Layout>
+      <p>Hello Next.js</p>
+    </Layout>
+  );
+}
