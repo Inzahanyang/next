9ba34702e0abcce592cd271666e91cf59d1922e4
inzahanyang <inzahanyang@gmai.com>
2020-02-27 21:33:13 +0900

    Layout as a Higher Order Component

--- 2-using-shared-components/pages/index.js
+++ 2-using-shared-components/pages/index.js
@@ -1,10 +1,5 @@
-import Link from 'next/link';
-import Layout from '../components/MyLayout';
+import withLayout from '../components/MyLayout';
 
-export default function Index() {
-  return (
-    <Layout>
-      <p>Hello kimyangwoo</p>
-    </Layout>
-  );
-}
+const Page = () => <p>Hello Next.js</p>;
+
+export default withLayout(Page);
